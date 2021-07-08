2e85c8e802f12f1752fbd3f79d96638ad7457688
Lukas Wölfinger <72506785+LukasWoelfinger@users.noreply.github.com>
2021-07-08 19:20:27 +0200

    cleanup unused and commented ouputs

--- IaC/Terraforming/02_separatedAreas/outputs.tf
+++ IaC/Terraforming/02_separatedAreas/outputs.tf
@@ -6,22 +6,10 @@ output "terraform_state" {
   value = "Stored at '${var.terraformstorageaccount}' at container '${var.terraformcontainername}' in file ${var.tags.Application}-terraform.tfstate"
 }
 
-# output "certificate_value" {
-#   value = data.azurerm_key_vault_secret.cert-base64.value
-# }
-
-# output "app_registration_name" {
-#   value = azuread_application.app_registration.name
-# }
-
 # Web output
 output "app_service_default_hostname" {
   value = "https://${azurerm_app_service.main.default_site_hostname}"
 }
-
-# output "certificate_thumbprint" {
-#   value = "${azurerm_key_vault_certificate.main.thumbprint}"
-# }
 
 # SQL outputs 
 output "sql-administrator_login" {
